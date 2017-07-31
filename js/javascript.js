738ec4bd93204c7caad745cf68afe2b8a35d04f2
natlef <nathalie.le59@numericable.fr>
2017-07-31 14:18:23 +0200

    premier commit

---
+++ js/javascript.js
@@ -0,0 +1,19 @@
+
+
+$ ( ".bouton_rouge" ).click(function() {
+	var valbtrouge = $( ".bouton_rouge").text();
+	if (valbtrouge == 'Show the article' )
+	{
+  		$ ( ".bouton_rouge" ).text('Hidden the article');
+  		$ ( ".lecture" ).css('visibility','visible') ;
+  		$ ( ".lecture" ).css('height','auto');
+  	}
+  	else
+  	{
+  		$ ( ".bouton_rouge" ).text('Show the article');
+  		$ ( ".lecture" ).css('visibility','Hidden');
+  		$ ( ".lecture" ).css('height',0);
+  	}
+
+});
+
